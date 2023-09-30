4f34183ebbbbd49e283f712ed6ffc72e05b24769
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2023-09-30 21:18:55 +0530

    sidebar and darkmode and lighmode theme changes compleate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/Team";
+// import Invoices from "./scenes/Invoices";
+// import Contacts from "./scenes/Contacts";
+// import Bar from "./scenes/Bar";
+// import Form from "./scenes/Form";
+// import Line from "./scenes/Line";
+// import Pie from "./scenes/Pie";
+// import Faq from "./scenes/Faq";
+// import Geography from "./scenes/Geography";
+// import Calender from "./scenes/Calender";
+import { Route, Routes } from "react-router-dom";
+
+
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -10,8 +25,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar/>
           <main className="content">
           <Topbar/>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            {/* <Route path="/team" element={<Team/>}/>
+            <Route path="/contacts" element={<Contacts/>}/>
+            <Route path="/invoices" element={<Invoices/>}/>
+            <Route path="/form" element={<Form/>}/>
+            <Route path="/bar" element={<Bar/>}/>
+            <Route path="/pie" element={<Pie/>}/>
+            <Route path="/line" element={<Line/>}/>
+            <Route path="/faq" element={<Faq/>}/>
+            <Route path="/geography" element={<Geography/>}/>
+            <Route path="/calender" element={<Calender/>}/> */}
+          </Routes>
           </main>
         </div>
       </ThemeProvider>
